d489c2fff8ff3fbbc0c3c16a5095881da777759e
dave8927 <dave.8927@gmail.com>
2021-04-27 23:02:09 +0000

    formatting

--- vapectrl.py
+++ vapectrl.py
@@ -14,7 +14,7 @@ def vape_power_on():
     try:
         for x in range(5):  # sets the number of times to repeat the loop
             GPIO.output(26, True)  # sets GPIO 26 to True, activating the relay
-                time.sleep(0.1)  # time to leave relay on
+            time.sleep(0.1)  # time to leave relay on
             GPIO.output(26, False)  # sets GPIO 26 to False, deactivating the relay
             time.sleep(0.1)  # time leave relay off before before looping
         GPIO.cleanup()
@@ -63,13 +63,11 @@ def vape_vape():
 
     GPIO.output(26, False)  # sets GPIO 26 starting state to False
 
-    # requested delay time in seconds t
-    delay_time = 5
+
+    delay_time = 5  # requested delay time in seconds
     print("Starting ", delay_time, " second delay:")
     print("Start Mass Flow Control...")
-    for x in range(
-        delay_time
-    ):  # range value is the number of times the loop will repeat
+    for x in range(delay_time):  # range is no. times the loop will repeat
         print(x + 1)  # range starts at 0, +1 makes it start at 1
         time.sleep(1)
 
